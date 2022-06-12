d7826b23bee90074d53faaafb2b3f47d80e68f1f
zephrodb <jeremy.fleming@gmail.com>
2022-06-12 12:30:11 -0600

    GUI and MQTT working. Time to make the magic happe

--- smartRVpythonic.py
+++ smartRVpythonic.py
@@ -1,23 +1,21 @@
 #!/usr/bin/python3
+#from email import message
 import logging
 from os import system
 from turtle import up
-import unittest
 import yaml
-import adafruit_dht
-import board
+#import adafruit_dht
 import RPi.GPIO as GPIO
 import smbus
 import sys
 import time
 from espeakng import ESpeakNG
-import random 
-import SDL_Pi_INA3221
-import influxdb_client
-from influxdb_client import Point, InfluxDBClient
-from influxdb_client.client.write_api import ASYNCHRONOUS
-import argparse
+#import random 
+#import argparse
 import paho.mqtt.client as mqtt
+import threading
+from tkinter import *
+
 
 class environmentThresholds:
     def __init__(self, highHumidity, highTemp, medHumidity, medTemp):
@@ -63,13 +61,11 @@ class radio:
         GPIO.output(self.radioCharger2_out, GPIO.HIGH)
         time.sleep(0.1)
         GPIO.output(self.ptt_out, GPIO.LOW)
-
     def pttUp(self):
         GPIO.output(self.ptt_out, GPIO.HIGH)
         time.sleep(0.1)
         GPIO.output(self.radioCharger1_out, GPIO.LOW)
         GPIO.output(self.radioCharger2_out, GPIO.LOW)
-
     def enableDTMF(self):
         self.DTMFOperStatus = True
     #this is a method on the radio object
@@ -155,69 +151,6 @@ class powerControl:
             logging.info(msg + " " + self.Name)
             return msg
 
-# class powerMonitor:
-#     def __init__(self, shuntAddress, shunt1name, shunt2name, shunt3name, influxBucket, influxURL, influxToken, influxORG):
-#         self.shuntAddress = shuntAddress
-#         self.shunt1name = shunt1name
-#         self.shunt2name = shunt2name
-#         self.shunt3name = shunt3name
-#         self.influxORG = influxORG
-#         self.influxToken = influxToken
-#         self.influxURL = influxURL
-#         self.influxBucket = influxBucket
-#         self.client = influxdb_client.InfluxDBClient(influxURL, influxToken, influxORG)
-#         self.write_api = self.client.write_api(write_options=ASYNCHRONOUS)
-#     def readBatteries(self):
-#         ina3221 = SDL_Pi_INA3221.SDL_Pi_INA3221(addr=0x43)
-#         busvoltage1 = ina3221.getBusVoltage_V(1)
-#         shuntvoltage1 = ina3221.getShuntVoltage_mV(1)
-#         current_mA1 = ina3221.getCurrent_mA(1)  
-#         loadvoltage1 = busvoltage1 + (shuntvoltage1 / 1000)
-#         busvoltage2 = ina3221.getBusVoltage_V(2)
-#         shuntvoltage2 = ina3221.getShuntVoltage_mV(2)
-#         current_mA2 = ina3221.getCurrent_mA(2)  
-#         loadvoltage2 = busvoltage2 + (shuntvoltage2 / 1000)
-#         busvoltage3 = ina3221.getBusVoltage_V(3)
-#         shuntvoltage3 = ina3221.getShuntVoltage_mV(3)
-#         current_mA3 = ina3221.getCurrent_mA(3)  
-#         loadvoltage3 = busvoltage3 + (shuntvoltage3 / 1000)
-#         system("clear")
-#         print(f"{busvoltage1}, {shuntvoltage1}, {current_mA1}, {loadvoltage1}")
-#         influx.writeInflux(self, measurment="shuntvoltage1", unit="Volt", value=shuntvoltage1)
-#         influx.writeInflux(self, measurment="current_mA1", unit="mA", value=current_mA1)
-#         print(f"{busvoltage2}, {shuntvoltage2}, {current_mA2}, {loadvoltage2}")
-#         influx.writeInflux(self, measurment="shuntvoltage2", unit="Volt", value=shuntvoltage2)
-#         influx.writeInflux(self, measurment="current_mA2", unit="mA", value=current_mA2)
-#         print(f"{busvoltage3}, {shuntvoltage3}, {current_mA3}, {loadvoltage3}")
-#         influx.writeInflux(self, measurment="shuntvoltage3", unit="Volt", value=shuntvoltage3)
-#         influx.writeInflux(self, measurment="current_mA3", unit="mA", value=current_mA3)
-
-
-# class influx:
-#     def __init__(self,  influxBucket, influxURL, influxToken, influxORG ):
-#         #self.InfluxDBClient = InfluxDBClient
-#         self.influxBucket = influxBucket
-#         #self.write_api = InfluxDBClient.write_api(self, write_options=ASYNCHRONOUS)
-#         self.client = influxdb_client.InfluxDBClient(influxURL, influxToken, influxORG)
-#         self.influxORG = influxORG
-#         self.influxToken = influxToken
-#         self.influxURL = influxURL
-#         self.write_api = self.client.write_api(write_options=ASYNCHRONOUS)
-#     def writeInflux(self, measurment, unit, value):
-#         self.measurment = measurment
-#         self.unit = unit
-#         self.value = value
-#         self.now = int(time.time_ns())
-#         write_json[0]['measurement'] = self.measurment
-#         write_json[0]['fields'][self.unit] = self.value
-#         write_json[0]['time'] = self.now
-#         print("Write points: {0}".format(write_json))
-#         self.write_api.write_points(write_json)
-#         print(self.data)
-#         self.write_api.write(bucket=self.influxBucket, org=self.influxORG, record=self.data)
-#         time.sleep(1)
-
-
 class mqtt_sub:
     def __init__(self, mqtt_feed, mqtt_host, mqtt_password, mqtt_port, mqtt_username):
         self.mqtt_feed = mqtt_feed
@@ -225,30 +158,220 @@ class mqtt_sub:
         self.mqtt_password = mqtt_password
         self.mqtt_port = mqtt_port
         self.mqtt_username = mqtt_username
-        self.client = mqtt.Client("digi_mqtt_test")  # Create instance of client with client ID “digi_mqtt_test”
+        
+    def connect(self):
+        self.client = mqtt.Client("rosebud_mqtt")  
         self.client.username_pw_set(self.mqtt_username, self.mqtt_password)
-        self.client.on_connect = on_connect  # Define callback function for successful connection
-        self.client.on_message = on_message  # Define callback function for receipt of a message
+        self.client.on_message = self.on_message
+        self.client.on_connect = self.on_connect
         self.client.connect(self.mqtt_host, self.mqtt_port)
-        self.client.loop_forever()  # Start networking daemon
-    def on_message(self):
-        msg = str(message.payload.decode("utf-8"))
-        if "HI=" in msg:
-            hi = msg.split("=")[2]
-            hi =  hi.strip(" '")
-            #button12.delete("1.0","end")
-            buttonTxt = "Heat Index = {}".format(hi)
-            print(buttonTxt)
-            button12=Button(text=buttonTxt)
-            #button12.update()
+        self.client.subscribe("/feeds/stats")
+        print("subscribing to topic : "+self.mqtt_feed)        
+        self.client.loop_start()
+        
+    def on_connect(self, client, userdata, flags, rc):
+        rc = rc
+        print("CONNECTED")
+        print("Connected with result code: ", str(rc))
+
+    
+    def on_message(self, client, userdata, msg):
+        payld = str(msg.payload)
+        if 't_0' in payld:
+            if 'temp_F' in payld:
+                temp_f = payld.split("=")[2]
+                temp_f = temp_f.strip(" '")
+                # print(temp_f)
+            elif "RH" in payld:
+                rh = payld.split("=")[2]   
+                rh = rh.strip(" '")
+                # print(rh)
+            elif "HI" in payld:
+                hi = payld.split("=")[2]   
+                hi = hi.strip(" '")
+
+                hi = str(hi)
+                # print(hi)
+                self.button12=Button(window, text="Heat Index = " +hi, height = 5, width = 15)
+                self.button12.grid(row=4,column=0)
+                gui.updateHI(self, hi)
+
+window = Tk()
+window.geometry('800x600')
+window.title("Glass Control Panel")
+T = Text(window, height = 1, width = 17)
+    
+
+class gui:
+    def __init__(self, window, hi, setTemp): 
+        mqtt_thread = threading.Thread(target=mqtt_sub.connect(mqtt_))
+        mqtt_thread.start()
+        mqtt_thread.join()
+        
+        self.hi = hi
+        self.setTemp = setTemp
+        self.buttonDown0 = 0
+        self.buttonDown1 = 0
+        self.buttonDown2 = 0
+        self.buttonDown3 = 0
+        self.buttonDown4 = 0
+        self.buttonDown5 = 0
+        self.buttonDown6 = 0
+        self.buttonDown7 = 0
+        self.buttonDown8 = 0        
+        self.buttonDown9 = 0
+        self.buttonDown10 = 0
+        self.buttonDown11 = 0
+        self.buttonDown12 = 0
+        self.buttonDown13 = 0
+
+        self.button0=Button(window, text="RV Enable",
+        command=self.rvEnable, height = 5, width = 15)
+        self.button0.grid(row=1,column=0)
+
+        self.button1=Button(window, text="Kitchen Light",
+        command=self.kitchenLight, height = 5, width = 15)
+        self.button1.grid(row=1,column=1)
+
+        self.button2=Button(window, text="Generator Start",
+        command=self.genStart, height = 5, width = 15)
+        self.button2.grid(row=1,column=2)
+
+        self.button3=Button(window, text="Exterior Lights",
+        command=self.outsideLights, height = 5, width = 15)
+        self.button3.grid(row=1,column=3)
+
+        self.button4=Button(window, text="Water Heater",
+        command=self.waterHeater, height = 5, width = 15)
+        self.button4.grid(row=2,column=0)
+
+        self.button5=Button(window, text="Water Pump",
+        command=self.waterPump, height = 5, width = 15)
+        self.button5.grid(row=2,column=1)
+
+        self.button6=Button(window, text="Step",
+        command=self.step, height = 5, width = 15)
+        self.button6.grid(row=2,column=2)
+
+        self.button7=Button(window, text="Porch Light",
+        command=self.porchLight, height = 5, width = 15)
+        self.button7.grid(row=2,column=3)
+
+        self.button8=Button(window, text="Inverter",
+        command=self.inverter, height = 5, width = 15)
+        self.button8.grid(row=3,column=0)
+
+        self.button9=Button(window, text="A/C Start",
+        command=self.acStart, height = 5, width = 15)
+        self.button9.grid(row=4,column=1)
+
+        self.button10=Button(window, text="Temp Down",
+        command=self.acDown, height = 5, width = 15)
+        self.button10.grid(row=4,column=2)
+
+        self.button11=Button(window, text="Temp Up",
+        command=self.acUp, height = 5, width = 15)
+        self.button11.grid(row=4,column=3)
+
+        self.button12=Button(window, text="Heat Index = " +hi, height = 5, width = 15)
+        self.button12.grid(row=4,column=0)
+
+        self.button13=Button(window, text=setTemp, height = 5, width = 15)
+        self.button13.grid(row=3,column=3)
 
 
+        window.mainloop()
 
-
+    def rvEnable(self):
+        if self.buttonDown0 == 0 :
+            self.buttonDown0 = 1
+            self.button0.configure(bg='green')
+        elif self.buttonDown0 == 1 :
+            self.button0.configure(bg='#D9D8D7')
+            self.buttonDown0 = 0
+    def kitchenLight(self):
+        if self.buttonDown1 == 0 :
+            self.buttonDown1 = 1
+            self.button1.configure(bg='green')
+        elif self.buttonDown1 == 1 :
+            self.button1.configure(bg='#D9D8D7')
+            self.buttonDown1 = 0
+    def genStart(self):
+        if self.buttonDown2 == 0 :
+            self.buttonDown2 = 1
+            self.button2.configure(bg='green')
+        elif self.buttonDown2 == 1 :
+            self.button2.configure(bg='#D9D8D7')
+            self.buttonDown2 = 0
+    def outsideLights(self):
+        if self.buttonDown3 == 0 :
+            self.buttonDown3 = 1
+            self.button3.configure(bg='green')
+        elif self.buttonDown3 == 1 :
+            self.button3.configure(bg='#D9D8D7')
+            self.buttonDown3 = 0
+    def waterHeater(self):
+        if self.buttonDown4 == 0 :
+            self.buttonDown4 = 1
+            self.button4.configure(bg='green')
+        elif self.buttonDown4 == 1 :
+            self.button4.configure(bg='#D9D8D7')
+            self.buttonDown4 = 0
+    def waterPump(self):
+        if self.buttonDown5 == 0 :
+            self.buttonDown5 = 1
+            self.button5.configure(bg='green')
+        elif self.buttonDown5 == 1 :
+            self.button5.configure(bg='#D9D8D7')
+            self.buttonDown5 = 0
+    def step(self):
+        if self.buttonDown6 == 0 :
+            self.buttonDown6 = 1
+            self.button6.configure(bg='green')
+        elif self.buttonDown6 == 1 :
+            self.button6.configure(bg='#D9D8D7')
+            self.buttonDown6 = 0
+    def porchLight(self):
+        if self.buttonDown7 == 0 :
+            self.buttonDown7 = 1
+            self.button7.configure(bg='green')
+        elif self.buttonDown7 == 1 :
+            self.button7.configure(bg='#D9D8D7')
+            self.buttonDown7 = 0
+    def inverter(self):
+        if self.buttonDown8 == 0 :
+            self.buttonDown8 = 1
+            self.button8.configure(bg='green')
+        elif self.buttonDown8 == 1 :
+            self.button8.configure(bg='#D9D8D7')
+            self.buttonDown8 = 0
+    def acStart(self):
+        if self.buttonDown9 == 0 :
+            self.buttonDown9 = 1
+            self.button9.configure(bg='green')
+        elif self.buttonDown9 == 1 :
+            self.button9.configure(bg='#D9D8D7')
+            self.buttonDown9 = 0
+    def acDown(self):
+        self.setTemp -= 1
+        self.button13['text'] = self.setTemp
+        #self.button13=Button(self.window, text=self.setTemp, height = 5, width = 15)
+    def acUp(self):
+        self.setTemp += 1
+        self.button13['text'] = self.setTemp
+    def updateHI(self, hi):
+        i_hi = float(hi)
+        i_hi = int(i_hi)
+        if i_hi >> 89 :
+            self.button12['text'] = "Heat Index = " +hi
+            self.button12['bg'] ='red'
+        elif i_hi <= 90 :
+            self.button12['text'] = "Heat Index = " +hi
+            self.button12['bg'] ='green'
 
 
 if __name__ == "__main__":
-
+    
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(levelname)s %(message)s',
                         filename='smartRV.log',
@@ -306,31 +429,17 @@ if __name__ == "__main__":
                                 configParams['gen2Enable_out'],
                                 configParams['chargerEnable_out'],
                                 configParams['relayHatBus'],
-                                configParams['relayHatAddress'])
-        write = influx(configParams['influxBucket'],
-                        configParams['influxURL'],
-                        configParams['influxToken'],
-                        configParams['influxORG'])     
+                                configParams['relayHatAddress'])    
         pins = setupPins(configParams['temp1_in'],
                         configParams['ptt_out'],
                         configParams['radioCharger1_out'],
                         configParams['radioCharger2_out'])  
-        #relays = setupRelays(configParams['temp1_in'],
-        #                    configParams['ptt_out'],
-        #                    configParams['radioCharger1_out'],
-        #                    configParams['radioCharger2_out'])     
-        mqtt_feed = mqtt_sub(configParams['mqtt_feed'],
+        mqtt_ = mqtt_sub(configParams['mqtt_feed'],
                             configParams['mqtt_host'],
                             configParams['mqtt_password'],
                             configParams['mqtt_port'],
                             configParams['mqtt_username'])            
-    
-    
-    
-    
-    
-    
-    
+        
     setupPins(22, 27, 20, 21)
     #setupRelays(relays)
     GPIO.output(20, GPIO.LOW)
@@ -338,17 +447,23 @@ if __name__ == "__main__":
     # make sure we arent dead keying
     GPIO.output(27, GPIO.HIGH)
     
+    # while True:
+    #     i=1
+    #     while i < 7 :
+    #         print()
+    #         i += 1
+    #     radio.sendCallsign(rad)
+    hi = "0"
+    setTemp = 82
+    gui(window, hi, setTemp)
+
+    mqtt_thread = threading.Thread(target=mqtt_sub.connect(mqtt_))
+    gui_thread = threading.Thread(target=gui(window, hi, setTemp))    
+    mqtt_thread.start()
+    gui_thread.start()
+    gui_thread.join()
+    mqtt_thread.join()
+
     
+
     
-    while True:
-        i=1
-        while i < 7 :
-            powerMonitor.readBatteries(shunts)
-            time.sleep(10)
-            print()
-            i += 1
-        radio.sendCallsign(rad)
-        
-
-
-
